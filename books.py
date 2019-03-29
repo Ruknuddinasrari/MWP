963a9d3443a0de74f64a088d1d5f2b81cc566a91
ruknuddin <ruknuddin.asrari@gmail.com>
2019-03-29 19:19:54 +0530

    add program 1 file

---
+++ books.py
@@ -0,0 +1,5 @@
+CREATE TABLE books (id INTEGER PRIMARY KEY,name TEXT,rating INTEGER);
+INSERT INTO books VALUES (1,"Safwan",21);
+INSERT INTO books VALUES (2,"maaz",22);
+INSERT INTO books VALUES (3,"amina",23);
+SELECT * FROM books;
